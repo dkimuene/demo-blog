168b6405cf78d299e6ddabf8d63a06579d3d7387
Deodat Kimuene <dkimuene1@gmail.com>
2022-07-27 18:25:20 -0500

    Added some stying to index and click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) =>{
+        console.log("Hello, my people", e)
+    }
+    
+    const handleClickAgain = (name, e) =>{
+        console.log(`Hello, ${name}`, e.target)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => {
+                handleClickAgain("Deo", e)
+            }}>Click Me Again</button>
         </div>
      );
 }
